c2f8b73827b7866ef783f07a5680b30090220a47
Corentin Dancette <corentin.dancette@gmail.com>
2020-07-01 00:10:49 +0200

    add grid

--- bootstrap/grid.py
+++ bootstrap/grid.py
@@ -13,12 +13,19 @@ def train_func(config):
     print("*****************" + os.getcwd())
     option_path = config.pop("option_file")
 
-    os.chdir(config.pop('run_dir'))
+    os.chdir(config.pop("run_dir"))
 
-    command = ["python", "-u", "-m", "bootstrap.run",
-        "-o", option_path,
-        "--exp.resume", "last",
-        "--exp.resume_or_start", "true",  # TODO : réfléchir à ce qu'on veut vraiment avoir ici..
+    command = [
+        "python",
+        "-u",
+        "-m",
+        "bootstrap.run",
+        "-o",
+        option_path,
+        "--exp.resume",
+        "last",
+        "--exp.resume_or_start",
+        "true",  # TODO : réfléchir à ce qu'on veut vraiment avoir ici..
     ]
 
     exp_dir = config.pop("exp_dir_prefix")
@@ -38,19 +45,15 @@ def train_func(config):
             value_str = ",".join(str(x) for x in value)
         else:
             value_str = str(value)
-        
+
         exp_dir += f"--{name.split('.')[-1]}_{value_str}"
-    
+
     command += ["--exp.dir", exp_dir]
     command += arguments
     print("************* COMMAND", command)
 
     subprocess.run(command, check=True)
-    
-    # for i in range(10):
-    #     train(model, optimizer, train_loader)
-    #     acc = test(model, test_loader)
-    #     tune.track.log(mean_accuracy=acc)  # TODO
+
 
 # TODO
 # analysis = tune.run(
@@ -58,26 +61,40 @@ def train_func(config):
 
 # print("Best config: ", analysis.get_best_config(metric="mean_accuracy"))
 
+
 def build_tune_config(option_path):
-    with open(option_path, 'r') as yaml_file:
+    with open(option_path, "r") as yaml_file:
         options = yaml.load(yaml_file)
     config = {}
-    for item in options['gridsearch']['params']:
-        key = item['name']
-        print("*************** VALUES", item['values'])
-        config[key] = tune.grid_search(item['values'])
+    for item in options["gridsearch"]["params"]:
+        key = item["name"]
+        print("*************** VALUES", item["values"])
+        config[key] = tune.grid_search(item["values"])
     print(config)
 
     config["exp_dir_prefix"] = options["exp"]["dir"]
     config["option_file"] = option_path
-    config['run_dir'] = os.getcwd()
-    return config, options["gridsearch"]["name"]
+    config["run_dir"] = os.getcwd()
+    return config, config["exp_dir_prefix"]
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--path_opts", required=True, help="Main file")
-    parser.add_argument("-g", "--gpu-per-trial", type=float, default=0.5, help="Percentage of gpu needed for one training")
-    parser.add_argument("-c", "--cpu-per-trial", type=float, default=2, help="Percentage of gpu needed for one training")
+    parser.add_argument(
+        "-g",
+        "--gpu-per-trial",
+        type=float,
+        default=0.5,
+        help="Percentage of gpu needed for one training",
+    )
+    parser.add_argument(
+        "-c",
+        "--cpu-per-trial",
+        type=float,
+        default=2,
+        help="Percentage of gpu needed for one training",
+    )
     args = parser.parse_args()
 
     config, name = build_tune_config(args.path_opts)
@@ -88,12 +105,10 @@ def main():
         name=name,
         # stop={"avg_inc_acc": 100},
         config=config,
-        resources_per_trial={
-            "cpu": args.cpu_per_trial,
-            "gpu": args.gpu_per_trial, 
-        },
+        resources_per_trial={"cpu": args.cpu_per_trial, "gpu": args.gpu_per_trial,},
         local_dir="ray_results",
     )
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
